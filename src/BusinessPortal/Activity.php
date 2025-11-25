7806270e9069a2a522717eec8a6cb3dc84280ba7
okang <okan.giritli@gmail.com>
2025-11-25 15:57:06 +0200

    Rename `data` to `properties` in `Activity` for improved property naming clarity.

--- src/BusinessPortal/Activity.php
+++ src/BusinessPortal/Activity.php
@@ -4,24 +4,24 @@ namespace Firebed\VatRegistry\BusinessPortal;
 
 class Activity extends Enum
 {
-    public function __construct(private array $data)
+    public function __construct(private array $properties)
     {
-        parent::__construct($data);
+        parent::__construct($properties);
     }
 
     public function getDateFrom(): ?string
     {
-        return $this->data['dtFrom'] ?? null;
+        return $this->properties['dtFrom'] ?? null;
     }
 
     public function getDateTo(): ?string
     {
-        return $this->data['dtTo'] ?? null;
+        return $this->properties['dtTo'] ?? null;
     }
 
     public function getType(): ?string
     {
-        return $this->data['type'] ?? null;
+        return $this->properties['type'] ?? null;
     }
 
     public function isPrimary(): bool
